124f361aa8af02a4c8c2626e3135759bd34801c0
sruthi <learning@DIN66009565.corp.capgemini.com>
2017-10-13 16:14:15 +0530

    first commit

---
+++ src/main/java/com/cg/App.java
@@ -0,0 +1,15 @@
+package com.cg;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+System.out.println( "This is modified" );
+
+    }
+}
